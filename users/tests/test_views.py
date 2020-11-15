c2d4d70c2b159d427ea081fc58fd3dd977bcfaac
Shivam Sharma <shivams906@gmail.com>
2020-11-15 19:17:38 +0530

    added retrieve, update and delete views and tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIRequestFactory, APITestCase
 from users.serializers import UserSerializer
-from users.views import UserList
+from users.views import UserList, UserDetail
 
 User = get_user_model()
 
@@ -31,3 +31,99 @@ class UserListViewTestCase(APITestCase):
             '', {'username': '', 'password': ''})
         response = UserList.as_view()(request)
         self.assertEqual(response.status_code, 400)
+
+
+class UserDetailViewTestCase(APITestCase):
+    def test_GET_returns_a_particular_user(self):
+        user = User.objects.create(username='test', password='test@123')
+        serializer = UserSerializer(user)
+        request = APIRequestFactory().get('')
+        response = UserDetail.as_view()(request, pk=user.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(serializer.data, response.data)
+
+    def test_PUT_replaces_all_data(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().put(
+            '', {'username': 'test1', 'password': 'test@321'})
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user.pk)
+        changed_user = User.objects.get(pk=user.pk)
+        serializer = UserSerializer(changed_user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(changed_user.username, 'test1')
+        self.assertEqual(changed_user.password, 'test@321')
+        self.assertEqual(serializer.data, response.data)
+
+    def test_PATCH_replaces_some_data(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().patch(
+            '', {'username': 'test1'})
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user.pk)
+        changed_user = User.objects.get(pk=user.pk)
+        serializer = UserSerializer(changed_user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(changed_user.username, 'test1')
+        self.assertEqual(changed_user.password, 'test@123')
+        self.assertEqual(serializer.data, response.data)
+
+    def test_unauthenticated_users_can_not_PUT(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().put(
+            '', {'username': 'test1', 'password': 'test@321'})
+        response = UserDetail.as_view()(request, pk=user.pk)
+        self.assertIn(response.status_code, [401, 403])
+        self.assertEqual(
+            response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_unauthenticated_users_can_not_PATCH(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().patch(
+            '', {'username': 'test1'})
+        response = UserDetail.as_view()(request, pk=user.pk)
+        self.assertIn(response.status_code, [401, 403])
+        self.assertEqual(
+            response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_DELETE_deletes_the_user_object(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().delete('')
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user.pk)
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_unauthenticated_users_can_not_DELETE(self):
+        user = User.objects.create(username='test', password='test@123')
+        request = APIRequestFactory().delete('')
+        response = UserDetail.as_view()(request, pk=user.pk)
+        self.assertIn(response.status_code, [401, 403])
+        self.assertEqual(
+            response.data['detail'], 'Authentication credentials were not provided.')
+
+    def test_user_can_PUT_to_own_data_only(self):
+        user = User.objects.create(username='test', password='test@123')
+        user2 = User.objects.create(username='test2', password='test@123')
+        request = APIRequestFactory().put(
+            '', {'username': 'testuser2', 'password': 'test@321'})
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user2.pk)
+        self.assertEqual(response.status_code, 403)
+
+    def test_user_can_PATCH_to_own_data_only(self):
+        user = User.objects.create(username='test', password='test@123')
+        user2 = User.objects.create(username='test2', password='test@123')
+        request = APIRequestFactory().patch(
+            '', {'username': 'testuser2'})
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user2.pk)
+        self.assertEqual(response.status_code, 403)
+
+    def test_user_can_DELETE_to_own_data_only(self):
+        user = User.objects.create(username='test', password='test@123')
+        user2 = User.objects.create(username='test2', password='test@123')
+        request = APIRequestFactory().delete('')
+        request.user = user
+        response = UserDetail.as_view()(request, pk=user2.pk)
+        self.assertEqual(response.status_code, 403)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .permissions import IsOwnerOrReadOnly
 from .serializers import UserSerializer
 
 User = get_user_model()
 
 
 class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
