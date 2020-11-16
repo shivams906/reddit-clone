bea764bd0cd011295557ca508523606e5a187bb5
Shivam Sharma <shivams906@gmail.com>
2020-11-16 20:48:11 +0530

    added 'subscriptions' field to serializer and tests

--- users/serializers.py
+++ users/serializers.py
@@ -5,9 +5,13 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    subscriptions = serializers.PrimaryKeyRelatedField(
+        allow_empty=False, many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'karma', 'friends')
+        fields = ('id', 'username', 'password',
+                  'karma', 'friends', 'subscriptions')
         read_only_fields = ('karma', 'friends', )
         extra_kwargs = {
             'password': {
--- users/tests/test_serializers.py
+++ users/tests/test_serializers.py
@@ -1,11 +1,34 @@
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
+from subreddits.models import Subreddit
 from users.serializers import UserSerializer
 
 User = get_user_model()
 
 
 class UserSerializerTestCase(APITestCase):
+    def test_representation_of_data(self):
+        user = User.objects.create(username='test', password='test@123')
+        serializer = UserSerializer(user)
+        self.assertEqual(serializer.data, {
+                         'id': str(user.id),
+                         'username': user.username,
+                         'karma': user.karma,
+                         'friends': list(user.friends.all()),
+                         'subscriptions': list(user.subscriptions.all())})
+
+    def test_subscriptions_can_not_be_edited_directly(self):
+        user = User.objects.create(username='test', password='test@123')
+        subreddit = Subreddit.objects.create(
+            name='test', description='sub for testing', admin=user)
+        serializer = UserSerializer(
+            data={'username': 'test2', 'password': 'test@123', 'subscriptions': [subreddit.pk, ]})
+        if serializer.is_valid():
+            serializer.save()
+        self.assertEqual(User.objects.count(), 2)
+        user2 = User.objects.get(username='test2')
+        self.assertEqual(user2.subscriptions.count(), 0)
+
     def test_can_not_view_password(self):
         user = User.objects.create(username='test', password='test@123')
         serializer = UserSerializer(user)
