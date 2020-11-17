6dcee6323d3756b1fc105a2f558098763e409a2d
Shivam Sharma <shivams906@gmail.com>
2020-11-17 15:15:24 +0530

    added post model and test

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from subreddits.models import Subreddit
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    subreddit = models.ForeignKey(
+        Subreddit, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
---
+++ posts/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from posts.models import Post
+from subreddits.models import Subreddit
+
+User = get_user_model()
+
+
+class PostModelTestCase(APITestCase):
+    def test_valid_data_creates_post(self):
+        user = User.objects.create(username='test', password='test@123')
+        subreddit = Subreddit.objects.create(
+            name='test', description='sub for testing', admin=user)
+        Post.objects.create(title='a post', subreddit=subreddit, author=user)
+        self.assertEqual(Post.objects.count(), 1)
