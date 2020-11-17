e02cdda11a8ffaed67b02447950675a40a15e894
Shivam Sharma <shivams906@gmail.com>
2020-11-17 21:25:37 +0530

    added post create view and tests

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,7 +1,10 @@
 from rest_framework.test import APIRequestFactory, APITestCase
 from posts.factories import PostFactory
+from posts.models import Post
 from posts.serializers import PostSerializer
 from posts.views import PostList
+from subreddits.factories import SubredditFactory
+from users.factories import UserFactory
 
 
 class PostListTestCase(APITestCase):
@@ -14,6 +17,28 @@ class PostListTestCase(APITestCase):
         request = APIRequestFactory().get('')
         response = PostList.as_view()(request)
 
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
         self.assertIn(post1_serializer.data, response.data)
         self.assertIn(post2_serializer.data, response.data)
+
+    def test_valid_POST_creates_post_object(self):
+        user = UserFactory()
+        subreddit = SubredditFactory()
+        request = APIRequestFactory().post(
+            '', data={'title': 'post 1', 'subreddit': subreddit.pk})
+        request.user = user
+        response = PostList.as_view()(request)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Post.objects.count(), 1)
+
+    def test_only_authenticated_users_can_POST(self):
+        subreddit = SubredditFactory()
+        request = APIRequestFactory().post(
+            '', data={'title': 'post 1', 'subreddit': subreddit.pk})
+        response = PostList.as_view()(request)
+
+        self.assertIn(response.status_code, [401, 403])
+        self.assertEqual(
+            response.data['detail'], 'Authentication credentials were not provided.')
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
