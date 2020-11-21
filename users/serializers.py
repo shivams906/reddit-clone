b746e1a7ee73dcddd0af5662c8c79acca2ab8e7b
Shivam Sharma <shivams906@gmail.com>
2020-11-21 15:07:38 +0530

    updated UserSerializer to save hashed passwords (not plain text like before)

--- users/serializers.py
+++ users/serializers.py
@@ -6,17 +6,31 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(
-        allow_empty=False, many=True, read_only=True)
+        allow_empty=False, many=True, read_only=True
+    )
     subscriptions = serializers.PrimaryKeyRelatedField(
-        allow_empty=False, many=True, read_only=True)
+        allow_empty=False, many=True, read_only=True
+    )
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password',
-                  'karma', 'posts', 'friends', 'subscriptions')
-        read_only_fields = ('karma', 'friends', )
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
+        fields = (
+            "id",
+            "username",
+            "password",
+            "karma",
+            "posts",
+            "friends",
+            "subscriptions",
+        )
+        read_only_fields = (
+            "karma",
+            "friends",
+        )
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
