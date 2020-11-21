1a1a26437bd7c4fb00fef1af291131680b09f72d
Shivam Sharma <shivams906@gmail.com>
2020-11-21 18:34:59 +0530

    added token authentication

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken import views
 from .views import UserList, UserDetail
 
 urlpatterns = [
     path("", UserList.as_view()),
     path("<uuid:pk>/", UserDetail.as_view()),
+    path("get-auth-token/", views.obtain_auth_token),
 ]
