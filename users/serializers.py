6d2e77a6a7483ec72a4607c99294f2bd41828612
Shivam Sharma <shivams906@gmail.com>
2020-11-21 19:18:55 +0530

    updated UserSerializer to save updated passwords as hashed too

--- users/serializers.py
+++ users/serializers.py
@@ -34,3 +34,10 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data["password"])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        instance = super().update(instance, validated_data)
+        if "password" in validated_data:
+            instance.set_password(validated_data["password"])
+            instance.save()
+        return instance
