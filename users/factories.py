a5906a81a446428b5eb6b7f8bc13a2bb09b51798
Shivam Sharma <shivams906@gmail.com>
2020-11-21 14:51:55 +0530

    changed UserFactory to use create_user

--- users/factories.py
+++ users/factories.py
@@ -6,5 +6,10 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Sequence(lambda n: f'user{n}')
-    password = 'test@123'
+    username = factory.Sequence(lambda n: f"user{n}")
+    password = "test@123"
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        manager = cls._get_manager(model_class)
+        return manager.create_user(*args, **kwargs)
